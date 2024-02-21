951176c736252630d74c043e4814b7328a1cdbb5
Brad Snyder <bsnyder13@gmail.com>
2024-02-20 21:32:43 -0500

    geolocation

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,1 +1,18 @@
-<h1>Forcast</h1>
+<h1>Forecast</h1>
+
+<script>
+    
+    const successCallback = (position) => {
+  console.log(position);
+};
+
+const errorCallback = (error) => {
+  console.log(error);
+};
+
+navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+
+    const baseURL = "https://api.weather.gov/points/"
+
+
+</script>
